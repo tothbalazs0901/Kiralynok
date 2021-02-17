4c2728ffd16e80213865a0747a1bbd5bd4aec7a7
tothbalazs <tbalazsmark@gmail.com>
2021-02-17 10:42:02 +0100

    OOP 6. feladatig

--- src/kiralynok/Kiralynok.java
+++ src/kiralynok/Kiralynok.java
@@ -2,15 +2,34 @@ package kiralynok;
 
 import java.io.IOException;
 
+
 public class Kiralynok {
-    public static void main(String[] args) throws IOException {        
+    private Tabla tabla;
+
+    public Kiralynok() {
+     tabla = new Tabla('#');
+     megoldasok();
+    }
+     public static void main(String[] args) throws IOException {    
+     Kiralynok kir =new Kiralynok();
+     kir.megoldasok();
+    }
+    
+    public void megoldasok(){
+        feladat4();
+        feladat6();
+    }
+    private void feladat4(){
         System.out.println("4. feladat: Az üres tábla megjelenítése: ");
         Tabla tabla = new Tabla('#');
         tabla.megjelenit(); 
-        
-        System.out.println("6. feladat: Feltöltött tábla:  ");
+    }
+    private void feladat6(){
+       System.out.println("6. feladat: Feltöltött tábla:  ");
         tabla.elhelyez(8);
         tabla.megjelenit();
     }
+    
+   
     
 }
