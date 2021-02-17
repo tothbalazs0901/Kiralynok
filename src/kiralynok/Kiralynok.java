4df69694d1adfb9ccec8e5ad205065f6ad7cb3a2
tothbalazs <tbalazsmark@gmail.com>
2021-02-17 10:26:37 +0100

    4.feladat kész

--- src/kiralynok/Kiralynok.java
+++ src/kiralynok/Kiralynok.java
@@ -3,8 +3,10 @@ package kiralynok;
 import java.io.IOException;
 
 public class Kiralynok {
-    public static void main(String[] args) throws IOException {
-        
+    public static void main(String[] args) throws IOException {        
+        System.out.println("4. feladat: Az üres tábla megjelenítése: ");
+        Tabla tabla = new Tabla('#');
+        tabla.megjelenit(); 
     }
     
 }
